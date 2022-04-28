dc1fcde17ea2e7d3371190b95c64a3588cb80938
karthikhegde05 <karthikhegde05@gmail.com>
2022-04-28 12:39:02 +0530

    playbook file update

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
 
     const authLogin = async (e) => {
         e.preventDefault();
+        console.log(axios.baseURL);
         const result = await axios.post("/login",
         {
             "userId": userId,
