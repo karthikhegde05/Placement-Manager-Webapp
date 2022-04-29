28641c5446fb2166781bbe48e9dbd0beffa2f7e8
karthikhegde05 <karthikhegde05@gmail.com>
2022-04-29 11:54:34 +0530

    nginx.conf changed

--- frontend/src/Api/axios.jsx
+++ frontend/src/Api/axios.jsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
+// export default axios.create({
+//     // baseURL: process.env.API_URL
+//     baseURL: "http://34.127.110.3:8082/"
+// });
+
 export default axios.create({
-    // baseURL: process.env.API_URL
-    baseURL: "http://34.127.110.3:8082/"
-});
+    baseURL: "/api/"
+})
