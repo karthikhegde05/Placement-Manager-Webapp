707a72cbb5b0cb4c4a4e90258b01efdd09b7fade
karthikhegde05 <karthikhegde05@gmail.com>
2022-05-06 15:49:46 +0530

    added logback-spring.xml

--- frontend/src/Api/axios.jsx
+++ frontend/src/Api/axios.jsx
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
-// export default axios.create({
-//     // baseURL: process.env.API_URL
-//     baseURL: "http://34.127.110.3:8082/"
-// });
+export default axios.create({
+    // baseURL: process.env.API_URL
+    baseURL: "http://34.127.110.3:8082/api/"
+});
 
 // export default axios.create({
 //     baseURL: "/api/"
 // });
 
-export default axios.create({
-    baseURL: "http://127.0.0.1:8082/api/"
-})
+// export default axios.create({
+//     baseURL: "http://127.0.0.1:8082/api/"
+// })
